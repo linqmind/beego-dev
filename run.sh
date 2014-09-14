2dc45e93810f7917d3289fe0b5c076355eebd5cf
zhaohaibin <zhaohaibin@outlook.com>
2014-09-14 14:02:43 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -e
 
-# USER=${USER:-"root"}
-# PASSWD=${PASSWD:-"root"}
 exec supervisord -c /etc/supervisor/supervisord.conf
-chmod u+x /root/repos/devrc/install-gorc.sh
-exec /usr/bin/zsh /root/repos/devrc/install-gorc.sh
-echo "hello,world"
